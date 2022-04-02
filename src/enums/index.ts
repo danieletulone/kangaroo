41f1c4d43b9a5092cc0c47790bd664d9de5ca062
danieletulone <danieletulone.work@gmail.com>
2022-04-02 21:29:52 +0200

    Add enums.

---
+++ src/enums/index.ts
@@ -0,0 +1,33 @@
+export enum Section {
+  DEVELOPERS = "fld",
+  STORAGE = "fls",
+  SHOP = "flsh",
+  PROJECT = "flp",
+}
+
+export enum StorageGateway {
+  LOCAL,
+  GOOGLE_DRIVE,
+  DROPBOX,
+  OTHER,
+}
+
+export enum DeveloperPage {
+  OPEN_SOURCE,
+  COMMUNITY,
+  DOCS,
+  GET_STARTED,
+}
+
+export enum ShopSection {
+  COLOURS,
+  STYLES,
+  PLUGINS,
+}
+
+export enum ProjectSection {
+  AUTHOR,
+  FEEDBACK,
+  REPORT,
+  GANTT,
+}
