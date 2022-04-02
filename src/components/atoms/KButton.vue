8dda2b7373b9d0bf795327de5596a539861fe2b0
danieletulone <danieletulone.work@gmail.com>
2022-04-02 21:30:57 +0200

    Add KButton and KLogo components.

---
+++ src/components/atoms/KButton.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup></script>
+
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<style>
+button {
+  background-color: #5f5f5f;
+  border-radius: 5px;
+  border: 1px solid #fff;
+  box-shadow: 0 3px 0 0 rgba(0, 0, 0, 0.1);
+  color: #fff;
+  cursor: pointer;
+  font-family: Nunito, sans-serif;
+  letter-spacing: 1px;
+  padding: 5px 15px;
+  text-transform: uppercase;
+  transition: 0.5s;
+}
+
+button:hover {
+  background: #383838;
+  box-shadow: 0 8px 5px -5px rgba(0, 0, 0, 0.1);
+  transform: translateY(-5px);
+}
+</style>
---
+++ src/components/atoms/KButtonCircle.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import { computed } from "vue";
+
+/**
+ * Component properties.
+ */
+const props = defineProps({
+  radius: {
+    type: Number,
+    default: 150,
+  },
+});
+
+/**
+ * Side, width or height, expressed in pixel.
+ */
+const sidePixels = computed(() => {
+  return props.radius * 2 + "px";
+});
+</script>
+
+<template>
+  <button class="flex-center">
+    <slot></slot>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+button {
+  width: v-bind(sidePixels);
+  height: v-bind(sidePixels);
+  background-color: #828282;
+  border-radius: 50%;
+}
+</style>
