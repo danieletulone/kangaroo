6c711bc8a9d4e041cb80484fc4408b79d8f05711
danieletulone <danieletulone.work@gmail.com>
2022-04-02 21:32:50 +0200

    Update router.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,23 +6,12 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView,
+      component: () => import("@/views/HomeView.vue"),
     },
     {
       path: "/dashboard",
       name: "dashboard",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/DashboardView.vue"),
-    },
-    {
-      path: "/shop",
-      name: "shop",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/ShopView.vue"),
+      component: () => import("@/views/DashboardView.vue"),
     },
   ],
 });
