45d26bba5ef70faedfc3f72ed3e4ed1f3fe8e659
danieletulone <danieletulone.work@gmail.com>
2022-04-07 21:55:04 +0200

    Add KHeader and KFooter to App.vue.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import KHeader from "@/components/organisms/headers/KHeader.vue";
+import KFooter from "@/components/organisms/footers/KFooter.vue";
 </script>
 
 <template>
+  <k-header />
+
   <RouterView />
+
+  <k-footer />
 </template>
