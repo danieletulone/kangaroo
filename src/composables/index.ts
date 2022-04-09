2c0eeeb160ce417a156e99293f3e246c3df361d4
danieletulone <danieletulone.work@gmail.com>
2022-04-09 11:13:55 +0200

    Add index.ts to composables folder.

---
+++ src/composables/index.ts
@@ -0,0 +1,3 @@
+export { useRootLink } from "@/composables/router/root-link.composable";
+export { default as useSimilarRoute } from "@/composables/router/similar-route.composable";
+export { useAutoHide } from "@/composables/style/autohide.composable";
