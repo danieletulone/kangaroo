0abaa6b42b63affe84ed62520fd795ca7dcd6b44
danieletulone <danieletulone.work@gmail.com>
2022-04-09 11:16:07 +0200

    Add NotFountView.vue and implement similarRoute composable.

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useSimilarRoute } from "@/composables";
+import { RouterLink } from "vue-router";
+
+const similarRoute = useSimilarRoute();
+</script>
+
+<template>
+  <div
+    class="container flex-center flex-column py-5"
+    style="height: calc(100vh - 100px)"
+  >
+    <h1>Not Found.</h1>
+    <p>
+      Maybe you search for:
+      <router-link :to="similarRoute" style="color: blue">
+        {{ similarRoute }}
+      </router-link>
+    </p>
+  </div>
+</template>
